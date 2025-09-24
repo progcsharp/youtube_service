7f4e604434d145517aeb1340b2fed0cc66f27dde
unknown <fonror@mail.ru>
2025-09-24 15:07:23 +0300

    fix connect database

--- db/config.py
+++ db/config.py
@@ -16,7 +16,7 @@ class DatabaseConfig:
         db_name = os.getenv("POSTGRES_DB", "fastapi_test")
         db_user = os.getenv("POSTGRES_USER", "postgres")
         db_password = os.getenv("POSTGRES_PASSWORD", "147896325")
-        db_host = os.getenv("POSTGRES_HOST", "postgres")
+        db_host = os.getenv("POSTGRES_HOST", "localhost")
         db_port = os.getenv("POSTGRES_PORT", "5432")
         self.database_url = (
             f"postgresql+asyncpg://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}"
