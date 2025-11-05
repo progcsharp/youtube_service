cb0a900ab47604553d2bd8a314fb22061e1dbc6d
unknown <fonror@mail.ru>
2025-11-05 19:01:14 +0300

    fix

--- routers/parser.py
+++ routers/parser.py
@@ -16,23 +16,6 @@ async def search(query: str):
 
 @router.get("/channel")
 async def channel(channel_id: str, account_id: UUID, db: AsyncSession = Depends(get_db)):
-    credentials_json = redis.get(f"credentials:{account_id}")
-
-    if credentials_json is None:
-        async with db() as session:
-            credentials_json = await get_credentials_by_channel_id(account_id, session)
-
-    credentials_dict = json.loads(credentials_json)
-    credentials = Credentials.from_authorized_user_info(credentials_dict)
-
-    youtube = build('youtube', 'v3', credentials=credentials)
-
-    response = youtube.channels().list(
-        part="snippet,contentDetails,statistics,status",
-        id=channel_id
-    ).execute()
-
-    return response
     # credentials_json = redis.get(f"credentials:{account_id}")
 
     # if credentials_json is None:
@@ -41,42 +24,58 @@ async def channel(channel_id: str, account_id: UUID, db: AsyncSession = Depends(
 
     # credentials_dict = json.loads(credentials_json)
     # credentials = Credentials.from_authorized_user_info(credentials_dict)
+
     # youtube = build('youtube', 'v3', credentials=credentials)
 
-    # # 1. Получаем uploads playlist ID
-    # channel_response = youtube.channels().list(
-    #     part="contentDetails",
+    # response = youtube.channels().list(
+    #     part="snippet,contentDetails,statistics,status",
     #     id=channel_id
     # ).execute()
 
-    # uploads_playlist_id = channel_response["items"][0]["contentDetails"]["relatedPlaylists"]["uploads"]
+    # return response
+    credentials_json = redis.get(f"credentials:{account_id}")
 
-    # # 2. Получаем все видео
-    # videos = []
-    # next_page_token = None
+    if credentials_json is None:
+        async with db() as session:
+            credentials_json = await get_credentials_by_channel_id(account_id, session)
 
-    # while True:
-    #     playlist_response = youtube.playlistItems().list(
-    #         part="snippet,contentDetails",
-    #         playlistId=uploads_playlist_id,
-    #         maxResults=50,
-    #         pageToken=next_page_token
-    #     ).execute()
+    credentials_dict = json.loads(credentials_json)
+    credentials = Credentials.from_authorized_user_info(credentials_dict)
+    youtube = build('youtube', 'v3', credentials=credentials)
 
-    #     videos.extend(playlist_response["items"])
-    #     next_page_token = playlist_response.get("nextPageToken")
-    #     if not next_page_token:
-    #         break
+    # 1. Получаем uploads playlist ID
+    channel_response = youtube.channels().list(
+        part="contentDetails",
+        forHandle=channel_id
+    ).execute()
 
-    # for item in videos:
-    #     video_id = item["contentDetails"]["videoId"]
-    #     video_response = youtube.videos().list(
-    #         part="snippet,statistics,contentDetails,status,player,recordingDetails,liveStreamingDetails,topicDetails",
-    #         id=video_id
-    #     ).execute()
-    #     item["statistics"] = video_response["items"][0]["statistics"]
-    #     item["contentDetails"] = video_response["items"][0]["contentDetails"]
-    #     item["status"] = video_response["items"][0]["status"]
+    uploads_playlist_id = channel_response["items"][0]["contentDetails"]["relatedPlaylists"]["uploads"]
 
-    # # 3. Возвращаем только нужные поля
-    # return videos
+    # 2. Получаем все видео
+    videos = []
+    next_page_token = None
+
+
+    playlist_response = youtube.playlistItems().list(
+        part="snippet,contentDetails",
+        playlistId=uploads_playlist_id,
+        maxResults=50,
+        pageToken=next_page_token
+    ).execute()
+
+    videos.extend(playlist_response["items"])
+    next_page_token = playlist_response.get("nextPageToken")
+
+
+    for item in videos:
+        video_id = item["contentDetails"]["videoId"]
+        video_response = youtube.videos().list(
+            part="snippet,statistics,contentDetails,status,player,recordingDetails,liveStreamingDetails,topicDetails",
+            id=video_id
+        ).execute()
+        item["statistics"] = video_response["items"][0]["statistics"]
+        item["contentDetails"] = video_response["items"][0]["contentDetails"]
+        item["status"] = video_response["items"][0]["status"]
+
+    # 3. Возвращаем только нужные поля
+    return videos
